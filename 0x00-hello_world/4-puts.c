4f7038892104a2be79ecc2220bf736c7176cf64e
mounirchebbi <2157@holbertonschool.com>
2020-09-16 18:41:33 +0100

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - entry point for the compiler
+*Return: 0 end of program
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+

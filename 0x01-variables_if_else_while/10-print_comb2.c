7c103a07188348c4c793f4ad5f2d3bdb85eb7dda
mounirchebbi <2157@holbertonschool.com>
2020-09-17 19:48:22 +0100

    test

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0 when success then exit
+ */
+int main(void)
+{
+	int i;
+	int j = 0;
+
+	do {
+		i = 0;
+		do {
+			putchar('0' + j);
+			putchar('0' + i);
+			putchar(',');
+			putchar(' ');
+			i++;
+		} while (i <= 9);
+		j++;
+	} while (j <= 9);
+	putchar('\n');
+	return (0);
+}

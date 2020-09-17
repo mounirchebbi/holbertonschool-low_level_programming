89643bb2f93da7fca628d201d626852f5ae6198f
mounirchebbi <2157@holbertonschool.com>
2020-09-17 09:14:37 +0100

    write to stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-
+#include <unistd.h>
 /**
 * main - entry point for the compiler
 *Return: 0 end of program
 */
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
 

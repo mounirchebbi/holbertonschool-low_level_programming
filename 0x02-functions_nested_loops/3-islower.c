aa0bc22fcdb85b914d674665cdad2ea1f9c470cb
mounirchebbi <2157@holbertonschool.com>
2020-09-22 10:53:18 +0100

    task4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+  *_islower - check if lower case
+  *Return: 1 when true 0 when false
+  */
+int _islower(int c)
+{
+	if ( c >= 97 && c<= 122 )
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

0d22cfce8f7ce9f02b3a2e09e499b58c8f95fce9
mounirchebbi <2157@holbertonschool.com>
2020-09-22 23:24:55 +0100

    task2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,16 +8,28 @@
  */
 int largest_number(int a, int b, int c)
 {
-if (a >= b && a > c)
+if (a > b && a > c)
 {
 return (a);
 }
-else if (b >= a && b > c)
+else if (b > a && b > c)
 {
 return (b);
 }
-else if (c >= a && c > b)
+else if (c > a && c > b)
 {
 return (c);
+}
+else if (a == c)
+{
+return (a);
+}
+else if (a == b)
+{
+return (a);
+}
+else
+{
+return (b);
 }
 }

e6b5b8c87b2b518e5911a92c291b863850909a0a
mounirchebbi <2157@holbertonschool.com>
2020-09-23 14:56:39 +0100

    fibunacci2

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+  *main- entry point
+  *Return: 0 when success then quit
+  */
+int main(void)
+{
+	unsigned long int pp = 1;
+	unsigned long int p = 2;
+	unsigned long int n = 3;
+	unsigned long int s = 2;
+
+	do {
+		pp = p;
+		p = n;
+		n = pp + p;
+		if (n % 2 == 0)
+		{
+			s += n;
+		}
+
+	} while (n < 4000000);
+
+	printf("%lu\n", n);
+	return (0);
+
+}

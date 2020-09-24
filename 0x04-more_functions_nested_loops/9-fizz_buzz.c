ac6814bfd685c6f9a1961f86ab3743f716ce3a54
mounirchebbi <2157@holbertonschool.com>
2020-09-24 20:40:24 +0100

    fizz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+/**
+  *main - entry point
+  *Return:0 when succes then exit
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (i % 3 == 0)
+		{
+			putchar('F');
+			putchar('i');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else if (i % 5 == 0)
+		{
+			putchar('B');
+			putchar('u');
+			putchar('z');
+			putchar('z');
+			putchar(' ');
+		}
+		else
+		{
+			printf("%d ", i);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+

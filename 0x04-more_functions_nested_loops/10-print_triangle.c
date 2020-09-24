5da802ba0a0d9c8db8949ea0e5da3ebd08ac5872
mounirchebbi <2157@holbertonschool.com>
2020-09-24 21:35:53 +0100

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+  *print_triangle - prints a triangle
+  *@size: triangle size
+  *Return : void
+  */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - i; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k < i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}

aecf678ebe83eaf373ab9186fd813cd9707c97c8
mounirchebbi <2157@holbertonschool.com>
2020-09-29 17:41:33 +0100

    str copy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+  *_strcpy - copies string
+  *@dest: pointer to destination array of char
+  *@src: string source
+  *Return: void
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; i < = 98; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}

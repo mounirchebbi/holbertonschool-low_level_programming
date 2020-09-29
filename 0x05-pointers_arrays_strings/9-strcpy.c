3e2d06fecf493df052ca7afd44aee772771bcaa0
mounirchebbi <2157@holbertonschool.com>
2020-09-29 18:03:12 +0100

    str copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,9 +9,11 @@ char *_strcpy(char *dest, char *src)
 {
 	int i;
 
-	for (i = 0 ; i < 98; i++)
+	while (*(src + i) != '\0')
 	{
 		*(dest + i) = *(src + i);
+		i++;
 	}
+	*(dest + i) = '\0';
 	return (dest);
 }

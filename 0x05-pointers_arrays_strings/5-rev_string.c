54f8701d3db4afd8ffa354d3c216b5aad20772c4
mounirchebbi <2157@holbertonschool.com>
2020-09-29 15:45:41 +0100

    rev*string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+  *print_rev - prints in reverse
+  *@s: pointer to string
+  *Return: void
+  */
+void rev_string(char *s)
+{
+	int i = 0;
+	int j;
+	char ch;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i / 2; j++)
+	{
+		ch = *(s + j);
+		*(s + j) = *(s + (i - j - 1));
+		*(s + (i - j - 1)) = ch;
+	}
+
+}

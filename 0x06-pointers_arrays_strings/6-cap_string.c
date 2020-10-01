e6f55bd62626aa958c6b1cf1a95bf719eb154b76
mounirchebbi <2157@holbertonschool.com>
2020-10-01 18:05:23 +0100

    words to upper

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+/**
+  *cap_string - words to uppercase
+  *@s: pointer to string
+  *Return: pointer to string
+  */
+char *cap_string(char *s)
+{
+	int i = 0;
+	int n, m;
+
+	while (*(s + i) != '\0')
+	{
+		n = (int) *(s + i);
+		if (n >= 97 && n <= 122)
+		{
+			if (i == 0)
+			*(s + i) = n - 32;
+			else
+			{
+				m = (int) *(s + (i - 1));
+				if (m == 32 || m == 33 || m == 34 || m == 40 || m == 41 || m == 46 || m == 59 || m == 63 || m == 123 || m == 125 || m == 10 || m == 9)
+					*(s + i) = n - 32;
+			}
+		}
+
+		i++;
+	}
+	return (s);
+}
+

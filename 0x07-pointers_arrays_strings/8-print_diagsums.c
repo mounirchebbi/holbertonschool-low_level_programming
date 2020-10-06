484c6a4e5dba94b7c6ae1c53292fca8a5d5489cb
mounirchebbi <2157@holbertonschool.com>
2020-10-06 17:24:48 +0100

    2d to 1d

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,11 +12,11 @@ void print_diagsums(int *a, int size)
 	long int sa = 0;
 	long int sb = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size; ++i)
 	{
 		sa += *(a + (i * (size + 1)));
 	}
-	for (j = size - 1; j > 0; j--)
+	for (j = size - 1; j > 0; --j)
 	{
 		sb += *(a + (i * (size - 1)));
 	}

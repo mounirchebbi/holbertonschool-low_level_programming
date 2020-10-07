3527c724296f7e253a4fc285b3f08fd97d4a0935
mounirchebbi <2157@holbertonschool.com>
2020-10-07 19:58:00 +0100

    recursive print

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+  *_puts_recursion- recursive print
+  *@s: char to print
+  *Return: void
+  */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	else
+	{
+		_putchar (*s);
+		s++;
+		_puts_recursion(s);
+	}
+}

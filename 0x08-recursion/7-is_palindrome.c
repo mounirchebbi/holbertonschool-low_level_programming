d7666b6d3ffe0890c6643bfe494c4f7ee27742ed
mounirchebbi <2157@holbertonschool.com>
2020-10-08 22:24:15 +0100

    test

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,5 +1,18 @@
 #include "holberton.h"
 /**
+  *lastchar- go to last char
+  *@s: first pointer
+  *Return: last char
+  */
+char *lastchar(char *s)
+{
+	char *stri = s;
+
+	if (*stri == '\0')
+		return (stri--);
+	return (lastchar(++stri));
+}
+/**
   *pal- search if palandrome
   *@s: pointer
   *@p: pointer
@@ -33,9 +46,7 @@ int is_palindrome(char *s)
 		return (0);
 	else if (*(s + 1) == '\0')
 		return (0);
-	for (p = s; *p > '\0'; p++)
-	{
-	}
+	p = lastchar (s);
 	p--;
 	return (pal(s, p));
 }

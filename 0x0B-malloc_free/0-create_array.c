753b8e30ff4e708ea728f4d12b77b185a86e7c7c
mounirchebbi <2157@holbertonschool.com>
2020-10-13 17:15:50 +0100

    malloc-0 : create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,8 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 
 	arr = malloc(sizeof(char) * size);
+	if (arr == NULL)
+		return (NULL);
 	if (size > 0)
 	{
 		for (i = 0; i < size; i++)

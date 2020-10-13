0143ea803bd32cd7daa3064950e076627689b7e4
mounirchebbi <2157@holbertonschool.com>
2020-10-13 17:24:44 +0100

    create array0

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,3 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -54,4 +52,3 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 char *create_array(unsigned int size, char c);
-#endif

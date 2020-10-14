06379f94151a4452f1b2d86a56535ecdd3cd931b
mounirchebbi <2157@holbertonschool.com>
2020-10-14 13:43:55 +0100

    reload 3

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  *stringsize - size of string
+  *@str: string
+  *@i: initialization
+  *Return: size
+  */
+int stringsize(char *str, int i)
+{
+	if (str == NULL)
+		return (0);
+	if (str[i] == '\0')
+		return (0);
+	return (1 + stringsize(str, ++i));
+}
+/**
+  *str_concat - concatenate 2 strings
+  *@s1: string1
+  *@s2: string2
+  *Return: string
+  */
+char *str_concat(char *s1, char *s2)
+{
+	int n1, n2, j;
+	int i = 0;
+	char *s12;
+
+	n1 = stringsize(s1, 0);
+	n2 = stringsize(s2, 0);
+	s12 = malloc(sizeof(char) * (n1 + n2 + 1));
+	for (; i < n1; i++)
+	{
+		s12[i] = s1[i];
+	}
+	for (j = i; j < n1 + n2; j++)
+	{
+		s12[j] = s2[j - i];
+	}
+	s12[i + j] = '\0';
+	return (s12);
+}

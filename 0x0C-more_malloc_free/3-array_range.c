f59dc9fd6e88aa08fac5998e4195d869fc2462f7
mounirchebbi <2157@holbertonschool.com>
2020-10-20 00:08:12 +0100

    task 4

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,7 +14,7 @@ int *array_range(int min, int max)
 	if (max < min)
 		return (NULL);
 	size = max - min;
-	arr = malloc(sizeof(int) * size);
+	arr = malloc(sizeof(int) * size + 1);
 	if (arr == NULL)
 		return (NULL);
 	if (size == 0)

85d0ad79d94af563d1c0d0c5135daaff24452dfb
mounirchebbi <2157@holbertonschool.com>
2020-10-20 14:25:17 +0100

    realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,8 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == 0)
 	{
-		if (ptr != NULL)
-			free(ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (old_size == 0 || ptr == NULL)

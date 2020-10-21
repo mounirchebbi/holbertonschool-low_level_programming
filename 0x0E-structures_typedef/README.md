50f5f0e0090988b49fc311dc6ff8c07e22215141
mounirchebbi <2157@holbertonschool.com>
2020-10-21 01:02:49 +0100

    doggo

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,5 @@
+Learning Objectives
+
+    What are structures, when, why and how to use them
+        How to use typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef TYPE_DOG
+#define TYPE_DOG
+/**
+  *struct dog - type dog
+  *@name: name
+  *@age: age
+  *@owner: owner
+  */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+#endif

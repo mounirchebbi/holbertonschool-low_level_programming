d2124cd2b782d1cd193abb231804fe43347b73d5
mounirchebbi <2157@holbertonschool.com>
2020-10-21 20:46:42 +0100

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+  *print_name - prints name depending on function parameter
+  *@name: string
+  *@f: pointer to function to use as parameter
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}

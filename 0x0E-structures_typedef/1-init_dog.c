01aa1c55ed2714b1c0a12ac890467e2c9e377348
mounirchebbi <2157@holbertonschool.com>
2020-10-21 01:09:49 +0100

    initialisation

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include "dog.h"
+/**
+  *init_dog - initialisation
+  *@d: pointer to variable type struct dog
+  *@name: name
+  *@age: age
+  *@owner: owner
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

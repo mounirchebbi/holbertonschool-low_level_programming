c782667968b79179199d2d06c67464fbe989c266
mounirchebbi <2157@holbertonschool.com>
2020-10-22 20:49:33 +0100

    data structure + functions def + functions selector + main

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,7 +16,7 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}};
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op) == *(s))
+		if (*(ops[i].op) == s[0] && s[1] == '\0')
 			return (ops[i].f);
 		i++;
 	}

4a0cb9099f1795889bcdf3cf5ac6835c34b9e64d
mounirchebbi <2157@holbertonschool.com>
2020-10-24 17:08:38 +0100

    variadic_3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+  *print_all - prints all arguments following format order
+  *@format: format
+  */
+void print_all(const char * const format, ...)
+{
+	int i, j;
+	va_list lst;
+	void *arg;
+	char *spacer = "";
+	specif spf[] = {
+		{"c", "%c"},
+		{"i", "%d"},
+		{"f", "%f"},
+		{"s", "%s"},
+		{NULL, NULL}
+	};
+
+	va_start(lst, format);
+	i = 0;
+	while ((format[i] != '\0') && format)
+	{
+		j = 0;
+		while (spf[j].a != NULL)
+		{
+			if (spf[j].a[0] == format[i])
+			{
+				printf("%s", spacer);
+				arg = va_arg(lst, void*);
+				if ((arg == NULL) && (spf[j].a[0] == 's'))
+					printf("%p", spf[j].a);
+				printf(spf[j].b, arg);
+				spacer = ", ";
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(lst);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,7 +1,18 @@
 #ifndef VARIADIC_HEADER
 #define VARIADIC_HEADER
+/**
+  *struct specifiers - %specifiers of format
+  *@a: %specifier
+  *@b: format
+  */
+typedef struct specifiers
+{
+	char * a;
+	char * b;
+} specif;
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 #endif

da474d75a3a228a56a8393d0007511b9a39a6827
mounirchebbi <2157@holbertonschool.com>
2020-10-30 16:34:52 +0100

    list lenght

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include<lists.h>
+/**
+  *list_len - list lenght
+  *@h: header of list
+  *Return: lenght
+  */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	if (!h)
+		return (0);
+	while (h)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}

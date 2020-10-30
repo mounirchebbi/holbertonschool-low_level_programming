b00f0532c097220e37fc841a5843bd54f88573a6
mounirchebbi <2157@holbertonschool.com>
2020-10-31 00:41:05 +0100

    add tail

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+/**
+  *charsize - size of string
+  *@s: string
+  *Return: size
+  */
+unsigned int charsize(const char *s)
+{
+	unsigned int i = 0;
+
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+  *add_node_end- add note to tail
+  *@head: pointer to list
+  *@str: string
+  *Return: size
+  */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *n = malloc(sizeof(list_t));
+	list_t *temp;
+
+	n->str = strdup(str);
+	n->len = charsize(str);
+	n->next = NULL;
+	if (*head == NULL)
+		*head = n;
+	else
+	{
+		temp = *head;
+		while (temp->next != NULL)
+			temp = temp->next;
+		temp->next = n;
+	}
+	return (*head);
+}

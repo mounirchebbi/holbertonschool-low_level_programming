f321a79f28686fabfaa422252672b367d05889a1
mounirchebbi <2157@holbertonschool.com>
2020-11-10 23:42:47 +0100

    copy file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,7 +52,8 @@ int main(int argc, char *argv[])
 	fd_out = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	buffer = allocate_buffer(argv[2]);
 	r = read(fd_in, buffer, 1024);
-	do {
+	while (r > -1)
+	{
 		if (fd_in == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
@@ -68,7 +69,7 @@ int main(int argc, char *argv[])
 		}
 		r = read(fd_out, buffer, 1024);
 		fd_out = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r != -1);
+	}
 	free(buffer);
 	close_file(fd_in);
 	close_file(fd_out);

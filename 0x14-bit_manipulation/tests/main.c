3fd37319956abbf37de37bd596f3ee9d4a885f8c
mounirchebbi <2157@holbertonschool.com>
2020-11-10 15:23:21 +0100

    set bit

--- 0x14-bit_manipulation/tests/main.c
+++ 0x14-bit_manipulation/tests/main.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	{
+		printf("n = %lu\n", n);
+		printf("n&1 = %lu\n", n&1);
+		print_binary(n >> 1);
+	}
+	putchar((n & 1) + '0');
+}
 int main(void)
 {
 	/*int i;
@@ -11,7 +21,9 @@ int main(void)
 		printf("*************************************************************\n");
 	}
 	return 0;*/
-	int a = sizeof(char);
-	printf("%d\n", a);
+	/*int a = sizeof(char);
+	printf("%d\n", a);*/
+	unsigned long int n = 8;
+	print_binary(n);
 	return 0;
 }

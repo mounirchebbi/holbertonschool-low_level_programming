6f5c337b6dbd80ae4886ded15e000a0dbc567cc4
mounirchebbi <2157@holbertonschool.com>
2020-12-14 23:06:15 +0100

    double linked list

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,44 +8,31 @@
 */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp;
 	dlistint_t *node;
-	dlistint_t *previous;
-	unsigned int pos = 0;
+	dlistint_t *temp = *h;
 
-	if (*h == NULL)
-		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-	temp = *h;
-	while (temp)
+	for (; idx != 1; idx--)
 	{
 		temp = temp->next;
-		pos++;
-	}
-	if (idx > pos)
-		return (NULL);
-	else if (idx == 0)
-		add_dnodeint(h, n);
-	else if (pos == idx)
-		add_dnodeint_end(h, n);
-	else
-	{
-		temp = *h;
-		pos = 0;
-		node = malloc(sizeof(dlistint_t));
-		if (node == NULL)
+		if (temp == NULL)
 			return (NULL);
-		node->n = n;
-		while (pos < idx)
-		{
-			previous = temp;
-			temp = temp->next;
-			pos++;
-		}
-		node->prev = previous;
-		node->next = temp;
-		previous->next = node;
-		temp->prev = node;
 	}
-	return (*h);
+
+	if (temp->next == NULL)
+		return (add_dnodeint_end(h, n));
+
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+
+	node->n = n;
+	node->prev = temp;
+	node->next = temp->next;
+	temp->next->prev = node;
+	temp->next = node;
+
+	return (node);
 }

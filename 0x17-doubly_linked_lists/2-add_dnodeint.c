63c4150d34918c30b199eae66e7488d13da1522b
mounirchebbi <2157@holbertonschool.com>
2020-12-14 22:46:01 +0100

    double linked list

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -10,6 +10,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *node;
 
 	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
 	node->n = n;
 	node->prev = NULL;
 	node->next = *head;

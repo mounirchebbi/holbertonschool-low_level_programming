0756ad60ef8e446ea3aa82b41a1e16a125ffb7ed
mounirchebbi <2157@holbertonschool.com>
2021-03-04 13:49:30 +0100

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,19 +8,17 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
+		hash_node_t *node;
 	char *value_copy;
-	unsigned long int i;
-	unsigned long int ind;
+	unsigned long int index, i;
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
-
 	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
-	ind = key_ind((const unsigned char *)key, ht->size);
-	for (i = ind; ht->array[i]; i++)
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
@@ -42,7 +40,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 	}
 	node->value = value_copy;
-	node->next = ht->array[ind];
-	ht->array[ind] = node;
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
